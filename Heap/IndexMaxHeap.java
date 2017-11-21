3732994a9751215aee992ad0c5c7ff29671b91aa
narlinen <narlinen@gmail.com>
2017-11-21 23:55:48 +0800

    更新IndexMaxHeap

--- Heap/IndexMaxHeap.java
+++ Heap/IndexMaxHeap.java
@@ -1,52 +1,93 @@
-public class IndexMaxHeap<Item extends Compare> {
-	private Item[] data;
+public class IndexMaxHeap<Item extends Comparable<Item>> {
+    private Item[] data;
 	private int[] indexes;
-	private int[] reverse;
-
-	private int count;
+	private int[] indexofindex;
+    private int count;
 	private int capacity;
 
 	public IndexMaxHeap(int capacity) {
-		this.data = new Item[capacity+1];
-		this.indexes = new int[capacity+1];
-		this.reverse = new int[capacity+1];
-        this.capacity = capacity;
-        this.count = 0;
+	    data = (Item[])new Object[capacity+1];
+		indexes = new int[capacity+1];
+		indexofindex = new int[capacity+1];
+		count = 0;
+		this.capacity = capacity;
 	}
 
 	public int size() {
-		return count;
+	    return count;
 	}
 
 	public boolean empty() {
-		return count == 0;
+	    return count == 0;
 	}
 
 	public void insert(int i,Item item) {
-
+	    i++;
+		data[i] = item;
+		indexes[++count] = i;
+		indexsindex[i] = count;
+		shiftUp(count);
 	}
 
 	public Item extractMax() {
-
+	    Item ret = data[indexes[1]];
+        swapIndex(1,count);
+		indexsindex[indexes[count--]] = 0;
+		shiftDown(1);
+		return ret;
 	}
 
 	public int extractMaxIndex() {
-
-	}
-
-    public void setItem(int i,Item item) {
-		
+	    int ret = indexes[1];
+		swapIndex(1,count);
+		indexsindex[indexes[count--]] = 0;
+		shiftDown(1);
+		return ret;
 	}
 
 	public Item getMax() {
-
+	    return data[indexes[1]];
 	}
 
 	public int getMaxIndex() {
-
+	    return indexesp[1] - 1;
 	}
 
 	public Item getItem(int i) {
+	    i++;
+		return data[i];
+	}
 
+	public void change(int i,Item item) {
+	
+	}
+
+	private void swapIndex(int i,int j) {
+	    int temp = indexes[i];
+		indexes[i] = indexes[j];
+		indexes[j] = temp;
+	}
+
+	private void shiftUp(int k) {
+	    while(k>1 && data[indexes[k]].compareTo(data[indexes[k/2]]) > 0) {
+		    swapIndex(k,k/2);
+			k /= 2;
+		}
+	}
+
+	private void shiftDown(int k) {
+	    while(k*2 <= count) {
+		    int i = k*2;
+			if(i+1 <= count && data[indexes[i+1]].compareTo(data[indexes[i]]) > 0) {
+			    i++;
+			}
+
+			if(data[indexes[k]].compareTo(data[indexes[i]]) > 0) {
+			    break;
+			}
+
+			swapIndex(i,k);
+			k = i;
+		}
 	}
 }
